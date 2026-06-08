9ed1f1ce9f3c80cc4d21beb6695dd43742489f9f
ladev-robot <1415698114@qq.com>
2026-06-08 13:59:19 +0800

    fixed: 修复markdown渲染跳转链接的问题

--- utils/markdownToHtml.ts
+++ utils/markdownToHtml.ts
@@ -2,6 +2,7 @@
 import remarkParse from "remark-parse";
 import remarkGfm from "remark-gfm";
 import remarkRehype from "remark-rehype";
+import rehypeRaw from "rehype-raw";
 import rehypeSlug from "rehype-slug";
 import rehypeStringify from "rehype-stringify";
 import { visit } from "unist-util-visit";
@@ -70,6 +71,7 @@ export default async function markdownToHtml(
     .use(remarkGfm)
     .use(remarkExtractHeadings)
     .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeRaw)
     .use(rehypeSlug)
     .use(rehypeWrapTables)
     .use(rehypeStringify)
